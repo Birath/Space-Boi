3250a3bd0e68b4245d466483be1c1cbc875eb4c6
AlterEgo <albelito98@gmail.com>
2018-03-16 16:08:35 +0100

    Multiplexor och mer saker i gamescreen classen tillagt, samt temporär kod för speltest

--- core/src/com/binarygames/spaceboi/screens/GameScreen.java
+++ core/src/com/binarygames/spaceboi/screens/GameScreen.java
@@ -1,10 +1,16 @@
 package com.binarygames.spaceboi.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.binarygames.spaceboi.SpaceBoi;
+import com.binarygames.spaceboi.entities.Player;
+import com.binarygames.spaceboi.input.PlayerInputProcessor;
 import com.binarygames.spaceboi.ui.GameUI;
 
 public class GameScreen implements Screen {
@@ -13,16 +19,34 @@ public class GameScreen implements Screen {
 
     private OrthographicCamera camera;
 
+    private PlayerInputProcessor inputProcessor;
+
     private GameUI gameUI;
 
+    private Player player;
+
+    //TEMPSTUFF(har tillhörande imports ovan)
+    private Texture img;
+
     public GameScreen(SpaceBoi game) {
+        //TEMPSTUFF
+        img = new Texture("playerShip.png");
+
         this.game = game;
 
         camera = new OrthographicCamera();
 
         gameUI = new GameUI();
 
-        Gdx.input.setInputProcessor(gameUI.getStage()); // TODO add multiplexer for player input
+        player = new Player(0,0);
+
+        inputProcessor = new PlayerInputProcessor(player);
+
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(gameUI.getStage());
+        multiplexer.addProcessor(inputProcessor);
+
+        Gdx.input.setInputProcessor(multiplexer);
     }
 
     private void update(float delta) {
@@ -37,6 +61,7 @@ public class GameScreen implements Screen {
 
     private void batchedDraw() {
         game.getBatch().begin();
+        game.getBatch().draw(img, player.getX(), player.getY());
         game.debugFont.draw(game.getBatch(), "GAME", 5, 20);
         game.getBatch().end();
     }
@@ -49,9 +74,8 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
         update(delta);
-
+        player.updateMovement(); //TEMP
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
         batchedDraw();
         draw();
     }
