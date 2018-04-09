d58d7a94f9159cc6cca95fdd6fb457b98bf1c20d
AlterEgo <albelito98@gmail.com>
2018-04-09 22:02:01 +0200

    Bodies och entities är nu uppdelade på ett nytt sätt, men i stort sätt samma funktionalitet borde vara kvar i övrigt

---
+++ core/src/com/binarygames/spaceboi/bodies/BaseDynamicBody.java
@@ -0,0 +1,29 @@
+package com.binarygames.spaceboi.bodies;
+
+import com.badlogic.gdx.physics.box2d.*;
+
+public abstract class BaseDynamicBody extends BaseBody {
+
+    protected Body body;
+
+    public BaseDynamicBody(World aWorld, float x, float y, float mass, float radius) {
+        super(aWorld, x, y, mass, radius);
+
+        //Skapar en body till denna sorts objekt
+        BodyDef groundBodyDef = new BodyDef();
+        groundBodyDef.type = BodyDef.BodyType.DynamicBody;
+        groundBodyDef.position.set(pos);
+        this.body = world.createBody(groundBodyDef);
+
+        CircleShape circleShape = new CircleShape();
+        circleShape.setRadius(rad);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = circleShape;
+        fixtureDef.density = 0.2f;
+        fixtureDef.friction = 0.4f;
+        fixtureDef.restitution = 0.8f;
+        Fixture fixture = body.createFixture(fixtureDef);
+        circleShape.dispose();
+    }
+}
