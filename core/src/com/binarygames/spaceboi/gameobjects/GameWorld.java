db4c410282fec910abe4f4418c8347f6e40edf30
Björn Birath <bjorn.birath@gmail.com>
2018-06-18 21:02:23 +0200

    Change so that all planets within range pulls entities

--- core/src/com/binarygames/spaceboi/gameobjects/GameWorld.java
+++ core/src/com/binarygames/spaceboi/gameobjects/GameWorld.java
@@ -42,7 +42,7 @@ public class GameWorld {
         addDynamicEntity(player);
         this.player = player;
 
-        Planet planet1 = new Planet(world, 10, 30, "moon.png",(float) Math.pow(3 * 10, 7), 100);
+        Planet planet1 = new Planet(world, 10, 30, "moon.png", (float) Math.pow(3 * 10, 7), 100);
         addStaticEntity(planet1);
         Planet planet2 = new Planet(world, 230, 30, "moon.png", (float) Math.pow(3 * 10, 7), 75);
         addStaticEntity(planet2);
@@ -72,15 +72,23 @@ public class GameWorld {
     }
 
     private void applyGravity(EntityDynamic entity) {
-
-
         Vector2 entityPos = entity.getBody().getPosition();
-        Planet closestPlanet = getClosestPlanet(entityPos);
-
+        ArrayList<Planet> planetsWithinRange = getPlanetsWithinGravityRange(entityPos);
+        Vector2 finalGravity = new Vector2();
+        for (Planet planet : planetsWithinRange) {
+            float angle = MathUtils.atan2(planet.getBody().getPosition().y - entityPos.y, planet.getBody().getPosition().x - entityPos.x);
+            Vector2 gravityPull = new Vector2(
+                    (float) (MathUtils.cos(angle) * GRAVITY_CONSTANT * planet.getMass() * entity.getMass() / planet.getRad()),
+                    (float) (MathUtils.sin(angle) * GRAVITY_CONSTANT * planet.getMass() * entity.getMass() / planet.getRad())
+            );
+            finalGravity.add(gravityPull);
+        }
+        entity.getBody().applyForceToCenter(finalGravity, true);
+        /*
         Vector2 closestPlanetPos = closestPlanet.getBody().getPosition();
         float distance = closestPlanetPos.dst(entityPos);
         if (closestPlanet.getRad() * Planet.GRAVITY_RADIUS >= distance) {
-            float angle = MathUtils.atan2(closestPlanetPos.y - entityPos.y, closestPlanetPos.x - entityPos.x);
+
             // Set constant gravity while inside a set radius
 
             // TODO Maybe change to not depend on planet radius. Not sure what else to use tho - Maybe remove radius and then change Gravity Constant? //Albin
@@ -88,24 +96,23 @@ public class GameWorld {
             float forceX = MathUtils.cos(angle) * (float) force;
             float forceY = MathUtils.sin(angle) * (float) force;
             entity.getBody().applyForceToCenter(forceX, forceY, true);
-
-            // TODO move to nice place
-            if (entity instanceof Player) {
-                Vector2 playerForce = new Vector2(forceX, forceY);
-                ((Player) entity).setPlayerAngle(playerForce.angle());
-            }
+        */
+        // TODO move to nice place
+        if (entity instanceof Player) {
+            // TODO Change to toPlanet vector instead
+            Vector2 playerForce = new Vector2(finalGravity);
+            ((Player) entity).setPlayerAngle(playerForce.angle());
         }
-
-
     }
 
-    private void removeBullets(List<EntityDynamic> toRemoveList){
+
+    private void removeBullets(List<EntityDynamic> toRemoveList) {
         Iterator<EntityDynamic> itr = toRemoveList.iterator();
 
-        while(itr.hasNext()){
+        while (itr.hasNext()) {
             EntityDynamic entity = itr.next();
-            if (entity instanceof Bullet){
-                if(((Bullet) entity).toRemove(player.getBody().getPosition().x, player.getBody().getPosition().y)){
+            if (entity instanceof Bullet) {
+                if (((Bullet) entity).toRemove(player.getBody().getPosition().x, player.getBody().getPosition().y)) {
                     world.destroyBody(entity.getBody());
                     itr.remove();
                 }
@@ -113,24 +120,19 @@ public class GameWorld {
         }
     }
 
-    private Planet getClosestPlanet(Vector2 bodyPos) {
-        float closestDistance = 0.0f;
-        Planet closestPlanet = null;
+    private ArrayList<Planet> getPlanetsWithinGravityRange(Vector2 bodyPos) {
+        ArrayList<Planet> planetsWithinRange = new ArrayList<>();
         for (EntityStatic entityStatic : staticEntities) { //Seems like we need to have a planetList since we want to apply gravity using every object in staticEntities
             if (Planet.class.isInstance(entityStatic)) {
                 Planet planet = (Planet) entityStatic;
                 Vector2 planetPos = planet.getBody().getPosition();
-                float distance = planetPos.dst(bodyPos) - planet.getRad();
-                if (closestDistance == 0.0f) {
-                    closestDistance = distance;
-                    closestPlanet = planet;
-                } else if (distance < closestDistance) {
-                    closestDistance = distance;
-                    closestPlanet = planet;
+                float distance = planetPos.dst(bodyPos);
+                if (planet.getRad() * Planet.GRAVITY_RADIUS >= distance) {
+                    planetsWithinRange.add(planet);
                 }
             }
         }
-        return closestPlanet;
+        return planetsWithinRange;
     }
 
     public void addDynamicEntity(EntityDynamic entity) {
