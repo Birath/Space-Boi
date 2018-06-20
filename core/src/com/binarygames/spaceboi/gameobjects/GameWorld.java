f82d87f28ab31031983062f575b3971a6b41ba53
Björn Birath <bjorn.birath@gmail.com>
2018-06-20 19:03:23 +0200

    Player angle is now relative to closest planet

--- core/src/com/binarygames/spaceboi/gameobjects/GameWorld.java
+++ core/src/com/binarygames/spaceboi/gameobjects/GameWorld.java
@@ -74,8 +74,8 @@ public class GameWorld {
     private void applyGravity(EntityDynamic entity) {
         Vector2 entityPos = entity.getBody().getPosition();
         ArrayList<Planet> planetsWithinRange = getPlanetsWithinGravityRange(entityPos);
+        Planet closetstPlanet = getClosestPlanet(planetsWithinRange, entityPos);
         if (entity.getEntityState() == ENTITY_STATE.STANDING) {
-            Planet closetstPlanet = getClosestPlanet(planetsWithinRange, entityPos);
             planetsWithinRange.clear();
             planetsWithinRange.add(closetstPlanet);
         }
@@ -103,10 +103,12 @@ public class GameWorld {
             entity.getBody().applyForceToCenter(forceX, forceY, true);
         */
         // TODO move to nice place
-        if (entity instanceof Player) {
+        if (entity instanceof Player && closetstPlanet != null) {
             // TODO Change to toPlanet vector instead
-            Vector2 playerForce = new Vector2(finalGravity);
-            ((Player) entity).setPlayerAngle(playerForce.angle());
+            player.setClosestPlanet(closetstPlanet);
+            Vector2 relativeVector = closetstPlanet.getBody().getPosition().sub(player.getBody().getPosition());
+            float angleToPlanet = MathUtils.atan2(relativeVector.y, relativeVector.x) * MathUtils.radiansToDegrees;
+            player.setPlayerAngle(angleToPlanet);
         }
     }
 
--- core/src/com/binarygames/spaceboi/gameobjects/entities/Player.java
+++ core/src/com/binarygames/spaceboi/gameobjects/entities/Player.java
@@ -21,6 +21,7 @@ public class Player extends EntityDynamic {
     private float playerAngle = 0f;
 
     private GameWorld gameWorld;
+    private Planet closestPlanet;
 
     public Player(World world, float x, float y, String path, float mass, float radius, GameWorld gameWorld, Weapon weapon) {
         super(world, x, y, path, mass, radius);
@@ -108,11 +109,13 @@ public class Player extends EntityDynamic {
         playerAngle = angle;
     }
 
-    @Override
-    public void hitPlanet(Planet planet) {
-        super.hitPlanet(planet);
+    public Planet getClosestPlanet() {
+        return closestPlanet;
     }
 
+    public void setClosestPlanet(final Planet closestPlanet) {
+        this.closestPlanet = closestPlanet;
+    }
 }
 
 
