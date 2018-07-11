6b98df8b435f261abb5e390ace0686b623e4019f
Gawkat <maximumname@gmail.com>
2018-07-11 18:35:31 +0200

    health pickup sound

--- core/src/com/binarygames/spaceboi/Assets.java
+++ core/src/com/binarygames/spaceboi/Assets.java
@@ -6,9 +6,7 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class Assets {
 
@@ -36,6 +34,7 @@ public class Assets {
     // Player
     public static final String PLAYER = "game/entities/player/norrland.jpg";
     public static final String PLAYER_WALK_ANIMATION = "game/entities/player/walk_animation/walk_animation_atlas.png";
+    public static final String PLAYER_PICKUP_HEALTH = "game/entities/player/health_pickup.mp3"; // https://freesound.org/people/SilverIllusionist/sounds/411172/
 
     // Weapons
     public static final String WEAPON_SHOTGUN_SHOT = "game/entities/weapons/shotgun/shot.mp3";
@@ -92,6 +91,7 @@ public class Assets {
         // Player
         assetManager.load(PLAYER, Texture.class);
         assetManager.load(PLAYER_WALK_ANIMATION, Texture.class);
+        assetManager.load(PLAYER_PICKUP_HEALTH, Sound.class);
 
         // Weapons
         assetManager.load(WEAPON_SHOTGUN_SHOT, Sound.class);
--- core/src/com/binarygames/spaceboi/gameobjects/pickups/HealthPickup.java
+++ core/src/com/binarygames/spaceboi/gameobjects/pickups/HealthPickup.java
@@ -3,6 +3,7 @@ package com.binarygames.spaceboi.gameobjects.pickups;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Filter;
+import com.binarygames.spaceboi.Assets;
 import com.binarygames.spaceboi.gameobjects.GameWorld;
 import com.binarygames.spaceboi.gameobjects.entities.Player;
 
@@ -20,6 +21,7 @@ public class HealthPickup extends Pickup {
     @Override
     public void onHit(Player player) {
         player.increaseHealth(HEALTH_INCREASE);
+        gameWorld.getGame().getSoundManager().play(Assets.PLAYER_PICKUP_HEALTH);
         Gdx.app.log("HealthPickup", "Player health: " + player.getHealth());
         remove = true;
     }
