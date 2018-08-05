6a911fd60d67e799b58ad708f7c7dda92c63fbe1
Björn Birath <bjorn.birath@gmail.com>
2018-08-05 20:45:27 +0200

    Boss is now stunned if it misses the player when charging

--- core/src/com/binarygames/spaceboi/gameobjects/entities/enemies/FinalBoss.java
+++ core/src/com/binarygames/spaceboi/gameobjects/entities/enemies/FinalBoss.java
@@ -10,24 +10,35 @@ import com.binarygames.spaceboi.gameobjects.entities.weapons.Cannon;
 import com.binarygames.spaceboi.gameobjects.entities.weapons.Weapon;
 
 public class FinalBoss extends Enemy implements MeleeEnemy {
+    // Cannon values
     public static final int MINIMUM_CANNON_DISTANCE = 500;
+    private static final float MINIMUM_TIME_SINCE_CHARGE = 0.5f;
+
+    // Charge values
+    private static final float MINIMUM_CHARGE_DISTANCE = 700;
 
     private static final float CHARGE_ATTACK_COOLDOWN = 10.0f;
-    private static final float MINIMUM_CHARGE_DISTANCE = 700;
+    private static final float MAXIMUM_CHARGE_TIME = 5.0f;
     private static final float CHARGE_CHANNEL_TIME = 2.0f;
-    private static final float CHARGE_SPEED = 30;
+    private static final float MISSED_CHARGE_STUN = 1.5f;
 
+    private static final float CHARGE_SPEED = 40;
+    private static final int CHARGE_DAMANGE = 100;
+
+
+    // Clocks
     private float timeSinceLastCharge = 0;
     private float timeSinceChannelStarted = 0;
+    private float timeSinceChargeStart = 0;
+    private float stunTime = 0;
     private float angle;
     private Cannon cannon;
+    private boolean stunned;
 
     private enum ChargeState {
         CHANNELING, CHARGING, COOLDOWN
     }
     private ChargeState chargeState = ChargeState.COOLDOWN;
-    private boolean channelingCharge = false;
-    private boolean charging = false;
 
 
     public FinalBoss(GameWorld gameWorld, float x, float y, String path) {
@@ -39,11 +50,8 @@ public class FinalBoss extends Enemy implements MeleeEnemy {
     @Override
     public void update(float delta) {
         super.update(delta);
-        if (chargeState == ChargeState.CHANNELING) {
-            timeSinceChannelStarted += delta;
-        } else if (chargeState == ChargeState.COOLDOWN) {
-            timeSinceLastCharge += delta;
-        }
+
+        // Rotate boss to stand on planet
         body.setTransform(body.getPosition(), angle * MathUtils.degreesToRadians + MathUtils.PI / 2);
     }
 
@@ -66,7 +74,15 @@ public class FinalBoss extends Enemy implements MeleeEnemy {
 
     @Override
     protected void updateAttacking(float delta) {
-        if (shouldStartCharging()) {
+        updateChargeTimers(delta);
+
+        if (stunned) {
+            // Play stun animation...
+            stunTime += delta;
+            standStill();
+            stunned = stunTime < MISSED_CHARGE_STUN;
+        }
+        else if (shouldStartChanneling()) {
             chargeState = ChargeState.CHANNELING;
             Gdx.app.log("FinalBoss", "Starting channel");
             standStill();
@@ -75,7 +91,7 @@ public class FinalBoss extends Enemy implements MeleeEnemy {
             charge();
         }
         else if (chargeState == ChargeState.CHANNELING) {
-           Gdx.app.log("FinalBoss", "Channeling charge");
+            Gdx.app.log("FinalBoss", "Channeling charge");
             standStill();
         }
 
@@ -88,9 +104,27 @@ public class FinalBoss extends Enemy implements MeleeEnemy {
         }
     }
 
+    private void updateChargeTimers(float delta) {
+        switch (chargeState) {
+            case CHANNELING:
+                timeSinceChannelStarted += delta;
+                break;
+            case COOLDOWN:
+                timeSinceLastCharge += delta;
+                break;
+            case CHARGING:
+                timeSinceChargeStart += delta;
+                break;
+        }
+    }
+
     private void charge() {
         if (chargeState == ChargeState.CHANNELING) {
             chargeState = ChargeState.CHARGING;
+        }
+        if (timeSinceChargeStart > MAXIMUM_CHARGE_TIME) {
+            stopCharge(false);
+            return;
         }
         Vector2 chargeVector = perpen.cpy();
         chargeVector.scl(CHARGE_SPEED);
@@ -101,16 +135,28 @@ public class FinalBoss extends Enemy implements MeleeEnemy {
         }
     }
 
+    private void stopCharge(boolean hitPlayer) {
+        if (!hitPlayer) {
+            stunned = true;
+        }
+        timeSinceLastCharge = 0;
+        timeSinceChannelStarted = 0;
+        timeSinceChargeStart = 0;
+        stunTime = 0;
+        chargeState = ChargeState.COOLDOWN;
+        Gdx.app.log("FinalBoss", "Should stop charge");
+    }
+
     private boolean shouldCharge() {
         return timeSinceChannelStarted > CHARGE_CHANNEL_TIME;
     }
 
-    private boolean shouldStartCharging() {
+    private boolean shouldStartChanneling() {
         return chargeState == ChargeState.COOLDOWN && timeSinceLastCharge > CHARGE_ATTACK_COOLDOWN && MINIMUM_CHARGE_DISTANCE > body.getPosition().dst2(player.getBody().getPosition());
     }
 
     private boolean shouldShootCannon() {
-        return MINIMUM_CANNON_DISTANCE > body.getPosition().dst2(player.getBody().getPosition());
+        return MINIMUM_CANNON_DISTANCE > body.getPosition().dst2(player.getBody().getPosition()) && timeSinceLastCharge > MINIMUM_TIME_SINCE_CHARGE;
     }
 
     @Override
@@ -118,6 +164,14 @@ public class FinalBoss extends Enemy implements MeleeEnemy {
         //moveAlongPlanet();
     }
 
+    @Override
+    public void touchedPlayer() {
+        if (chargeState == ChargeState.CHARGING) {
+            stopCharge(true);
+            gameWorld.getPlayer().reduceHealth(CHARGE_DAMANGE);
+            // Play melee animation
+        }
+    }
 
     private void shoot(Weapon shootWeapon) {
         if (!shootWeapon.canShoot()) {
@@ -141,14 +195,5 @@ public class FinalBoss extends Enemy implements MeleeEnemy {
         return angle;
     }
 
-    @Override
-    public void touchedPlayer() {
-        if (chargeState == ChargeState.CHARGING) {
-            timeSinceLastCharge = 0;
-            timeSinceChannelStarted = 0;
-            chargeState = ChargeState.COOLDOWN;
-            Gdx.app.log("FinalBoss", "Should stop charge");
-            // Play melee animation
-        }
-    }
+
 }
