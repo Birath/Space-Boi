18ae7395590ab427719ba25a22351fa67c4f4cd5
Björn Birath <bjorn.birath@gmail.com>
2019-06-19 22:41:37 +0200

    disable launchpad collisions

--- core/src/com/binarygames/spaceboi/gameobjects/EntityContactListener.java
+++ core/src/com/binarygames/spaceboi/gameobjects/EntityContactListener.java
@@ -40,28 +40,28 @@ public class EntityContactListener implements ContactListener {
 
 
         //Player x Planet
-        if (Planet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Player.class.isInstance(fixtureB.getBody().getUserData())) {
+        if (fixtureA.getBody().getUserData() instanceof Planet &&
+                fixtureB.getBody().getUserData() instanceof Player) {
             Player player = (Player) fixtureB.getBody().getUserData();
             Planet planet = (Planet) fixtureA.getBody().getUserData();
             player.hitPlanet(planet);
-        } else if (Player.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Planet.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Player &&
+                fixtureB.getBody().getUserData() instanceof Planet) {
             Player player = (Player) fixtureA.getBody().getUserData();
             Planet planet = (Planet) fixtureB.getBody().getUserData();
             player.hitPlanet(planet);
         }
         //Player x Bullet
-        else if (Bullet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Player.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof Bullet &&
+                fixtureB.getBody().getUserData() instanceof Player) {
             Bullet bullet = (Bullet) fixtureA.getBody().getUserData();
             Player player = (Player) fixtureB.getBody().getUserData();
             if (!bullet.getShooter().equals(player)) {
                 player.reduceHealth(bullet.getDamage());
                 bullet.setHasHit(true); //Remove bullets only if they dont hit yourself
             }
-        } else if (Player.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Bullet.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Player &&
+                fixtureB.getBody().getUserData() instanceof Bullet) {
             Bullet bullet = (Bullet) fixtureB.getBody().getUserData();
             Player player = (Player) fixtureA.getBody().getUserData();
             if (!bullet.getShooter().equals(player)) {
@@ -70,30 +70,30 @@ public class EntityContactListener implements ContactListener {
             }
         }
         //Player x Melee enemy
-        else if (MeleeEnemy.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Player.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof MeleeEnemy &&
+                fixtureB.getBody().getUserData() instanceof Player) {
             MeleeEnemy meleeEnemy = (MeleeEnemy) fixtureA.getBody().getUserData();
             meleeEnemy.touchedPlayer();
-        } else if (Player.class.isInstance(fixtureA.getBody().getUserData()) &&
-                MeleeEnemy.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Player &&
+                fixtureB.getBody().getUserData() instanceof MeleeEnemy) {
             MeleeEnemy meleeEnemy = (MeleeEnemy) fixtureB.getBody().getUserData();
             meleeEnemy.touchedPlayer();
         }
 
 
         //Enemy x Planet
-        else if (Planet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Enemy.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof Planet &&
+                fixtureB.getBody().getUserData() instanceof Enemy) {
             Enemy enemy = (Enemy) fixtureB.getBody().getUserData();
             enemy.hitPlanet((Planet) fixtureA.getBody().getUserData());
-        } else if (Enemy.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Planet.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Enemy &&
+                fixtureB.getBody().getUserData() instanceof Planet) {
             Enemy enemy = (Enemy) fixtureA.getBody().getUserData();
             enemy.hitPlanet((Planet) fixtureB.getBody().getUserData());
         }
         //Enemy x Bullet
-        else if (Bullet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Enemy.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof Bullet &&
+                fixtureB.getBody().getUserData() instanceof Enemy) {
             Bullet bullet = (Bullet) fixtureA.getBody().getUserData();
             Enemy enemy = (Enemy) fixtureB.getBody().getUserData();
             if (!bullet.getShooter().equals(enemy)) {
@@ -101,8 +101,8 @@ public class EntityContactListener implements ContactListener {
                 bullet.applyLifeSteal();
                 bullet.setHasHit(true);
             }
-        } else if (Enemy.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Bullet.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Enemy &&
+                fixtureB.getBody().getUserData() instanceof Bullet) {
             Bullet bullet = (Bullet) fixtureB.getBody().getUserData();
             Enemy enemy = (Enemy) fixtureA.getBody().getUserData();
             if (!bullet.getShooter().equals(enemy)) {
@@ -113,53 +113,53 @@ public class EntityContactListener implements ContactListener {
         }
 
         //Bullet x Planet
-        else if (Planet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Bullet.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof Planet &&
+                fixtureB.getBody().getUserData() instanceof Bullet) {
             Bullet bullet = (Bullet) fixtureB.getBody().getUserData();
             bullet.setHasHit(true);
-        } else if (Bullet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Planet.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Bullet &&
+                fixtureB.getBody().getUserData() instanceof Planet) {
             Bullet bullet = (Bullet) fixtureA.getBody().getUserData();
             bullet.setHasHit(true);
         }
 
         // Pickup x Player
-        else if (Pickup.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Player.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof Pickup &&
+                fixtureB.getBody().getUserData() instanceof Player) {
             Pickup pickup = (Pickup) fixtureA.getBody().getUserData();
             Player player = (Player) fixtureB.getBody().getUserData();
             pickup.onHit(player);
 
-        } else if (Player.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Pickup.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Player &&
+                fixtureB.getBody().getUserData() instanceof Pickup) {
             Pickup pickup = (Pickup) fixtureB.getBody().getUserData();
             Player player = (Player) fixtureA.getBody().getUserData();
             pickup.onHit(player);
         }
         // Homingrocket x Bullet
-        else if (Bullet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                HomingRocket.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof Bullet &&
+                fixtureB.getBody().getUserData() instanceof HomingRocket) {
             Bullet bullet = (Bullet) fixtureA.getBody().getUserData();
             bullet.setHasHit(true);
             HomingRocket homingRocket = (HomingRocket) fixtureB.getBody().getUserData();
             if (!(bullet instanceof HomingRocket)) {
                 homingRocket.setHasHit(true); //Did not work
             }
-        } else if (HomingRocket.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Bullet.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof HomingRocket &&
+                fixtureB.getBody().getUserData() instanceof Bullet) {
             Bullet bullet = (Bullet) fixtureB.getBody().getUserData();
             bullet.setHasHit(true);
             HomingRocket homingRocket = (HomingRocket) fixtureA.getBody().getUserData();
             if (!(bullet instanceof HomingRocket)) {
                 homingRocket.setHasHit(true);
             }
-        } else if (LaunchPad.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Player.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof LaunchPad &&
+                fixtureB.getBody().getUserData() instanceof Player) {
             LaunchPad launchPad = (LaunchPad) fixtureA.getBody().getUserData();
             Player player = (Player) fixtureB.getBody().getUserData();
             player.hitLauchPad(launchPad);
-        } else if (LaunchPad.class.isInstance(fixtureB.getBody().getUserData()) &&
-                Player.class.isInstance(fixtureA.getBody().getUserData())) {
+        } else if (fixtureB.getBody().getUserData() instanceof LaunchPad &&
+                fixtureA.getBody().getUserData() instanceof Player) {
             LaunchPad launchPad = (LaunchPad) fixtureB.getBody().getUserData();
             Player player = (Player) fixtureA.getBody().getUserData();
             player.hitLauchPad(launchPad);
@@ -173,36 +173,36 @@ public class EntityContactListener implements ContactListener {
         if (fixtureA.getBody().getUserData() == null || fixtureB.getBody().getUserData() == null) return;
 
         //Player x Planet
-        if (Planet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Player.class.isInstance(fixtureB.getBody().getUserData())) {
+        if (fixtureA.getBody().getUserData() instanceof Planet &&
+                fixtureB.getBody().getUserData() instanceof Player) {
             Player player = (Player) fixtureB.getBody().getUserData();
             if (!player.isChained()) {
                 player.leftPlanet();
             }
-        } else if (Player.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Planet.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Player &&
+                fixtureB.getBody().getUserData() instanceof Planet) {
             Player player = (Player) fixtureA.getBody().getUserData();
             if (!player.isChained()) {
                 player.leftPlanet();
             }
         }
         //Enemy x Planet
-        else if (Planet.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Enemy.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof Planet &&
+                fixtureB.getBody().getUserData() instanceof Enemy) {
             Enemy enemy = (Enemy) fixtureB.getBody().getUserData();
             enemy.leftPlanet();
-        } else if (Enemy.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Planet.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Enemy &&
+                fixtureB.getBody().getUserData() instanceof Planet) {
             Enemy enemy = (Enemy) fixtureA.getBody().getUserData();
             enemy.leftPlanet();
         }
         //Chaser x Player
-        else if (Player.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Chaser.class.isInstance(fixtureB.getBody().getUserData())) {
+        else if (fixtureA.getBody().getUserData() instanceof Player &&
+                fixtureB.getBody().getUserData() instanceof Chaser) {
             Chaser chaser = (Chaser) fixtureB.getBody().getUserData();
             chaser.stoppedTouchingPlayer();
-        } else if (Chaser.class.isInstance(fixtureA.getBody().getUserData()) &&
-                Player.class.isInstance(fixtureB.getBody().getUserData())) {
+        } else if (fixtureA.getBody().getUserData() instanceof Chaser &&
+                fixtureB.getBody().getUserData() instanceof Player) {
             Chaser chaser = (Chaser) fixtureA.getBody().getUserData();
             chaser.stoppedTouchingPlayer();
         }
@@ -212,24 +212,24 @@ public class EntityContactListener implements ContactListener {
     public void preSolve(Contact contact, Manifold oldManifold) {
         Fixture fixtureA = contact.getFixtureA();
         Fixture fixtureB = contact.getFixtureB();
-        if (Pickup.class.isInstance(fixtureA.getBody().getUserData())) {
-            if (!Planet.class.isInstance(fixtureB.getBody().getUserData())) {
+        if (fixtureA.getBody().getUserData() instanceof Pickup) {
+            if (!(fixtureB.getBody().getUserData() instanceof Planet)) {
                 contact.setEnabled(false);
             } else {
                 ((Pickup)fixtureA.getBody().getUserData()).setHitPlane(true);
                 contact.setEnabled(true);
             }
-        } else if (Pickup.class.isInstance(fixtureB.getBody().getUserData())) {
-            if (!Planet.class.isInstance(fixtureA.getBody().getUserData())) {
+        } else if (fixtureB.getBody().getUserData() instanceof Pickup) {
+            if (!(fixtureA.getBody().getUserData() instanceof Planet)) {
                 contact.setEnabled(false);
             } else {
                 ((Pickup)fixtureB.getBody().getUserData()).setHitPlane(true);
                 contact.setEnabled(true);
             }
-
+        } else if (fixtureA.getBody().getUserData() instanceof LaunchPad ||
+                fixtureB.getBody().getUserData() instanceof LaunchPad) {
+            contact.setEnabled(false);
         }
-
-
     }
 
     @Override
