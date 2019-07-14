41a6526a014e1de61f2124db2d04557258ffdbb6
Björn Birath <bjorn.birath@gmail.com>
2019-07-14 18:30:57 +0200

    fix broken commit

--- core/src/com/binarygames/spaceboi/gameobjects/entities/enemies/Shooter.java
+++ core/src/com/binarygames/spaceboi/gameobjects/entities/enemies/Shooter.java
@@ -1,5 +1,11 @@
 package com.binarygames.spaceboi.gameobjects.entities.enemies;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.binarygames.spaceboi.Assets;
 import com.binarygames.spaceboi.gameobjects.GameWorld;
@@ -8,12 +14,41 @@ import com.binarygames.spaceboi.gameobjects.entities.weapons.Weapon;
 
 public class Shooter extends Enemy {
 
+    private static final float WEAPON_WIDTH = 3;
+    private static final float WEAPON_HEIGHT = 14;
+
     private Machinegun machinegun;
+    private Sprite machinGunSprite;
 
     public Shooter(GameWorld gameWorld, float x, float y, String path) {
         super(gameWorld, x, y, path, EnemyType.SHOOTER);
 
         this.machinegun = new Machinegun(gameWorld, this);
+        machinGunSprite = new Sprite(gameWorld.getGame().getAssetManager().get(Assets.WEAPON_MACHINEGUN, Texture.class));
+        machinGunSprite.setSize(machinGunSprite.getWidth() * 0.1f, machinGunSprite.getHeight() * 0.1f);
+        machinGunSprite.setOriginCenter();
+        machinGunSprite.flip(true, false);
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch, OrthographicCamera camera) {
+        aim(batch);
+        super.render(batch, camera);
+    }
+
+    private void aim(SpriteBatch batch) {
+        Vector2 spritePosition = new Vector2(body.getPosition().x * PPM - machinGunSprite.getWidth() / 2, (body.getPosition().y)  * PPM - machinGunSprite.getHeight() / 2);
+        // Gets the sprites position in physics units and gets the angle from it to the player
+        float angle = getAngleToPlayer(body.getPosition());
+        machinGunSprite.setPosition(spritePosition.x, spritePosition.y);
+        machinGunSprite.setRotation(angle * MathUtils.radiansToDegrees);
+        if (!sprite.isFlipX() && !machinGunSprite.isFlipY()) {
+            machinGunSprite.flip(false, true);
+        } else if (sprite.isFlipX() && machinGunSprite.isFlipY()) {
+            machinGunSprite.flip(false, true);
+        }
+        machinGunSprite.draw(batch);
     }
 
     @Override
@@ -86,6 +121,18 @@ public class Shooter extends Enemy {
 
         Vector2 shootFrom = new Vector2(body.getPosition().x * PPM + perpen.x,
             body.getPosition().y * PPM + perpen.y);
+
+        Vector2 muzzle = new Vector2(WEAPON_WIDTH, WEAPON_HEIGHT).scl(1, 1);
+        if  (machinGunSprite.isFlipY()) {
+            muzzle.scl(1, -1);
+        }
+        //Gdx.app.log("Shooter", "Angle: " + getAngleToPlayer(body.getPosition()) * MathUtils.radiansToDegrees);
+        //Gdx.app.log("Shooter", "Angle: " + getAngleToPlayer(new Vector2(0, 0)) * MathUtils.radiansToDegrees);
+        muzzle.rotate(getAngleToPlayer(body.getPosition()) * MathUtils.radiansToDegrees + 90);
+        //getAngleToPlayer(new Vector2(0, 0));
+        //player.getBody().getPosition();
+        shootFrom.add(muzzle);
+
         if (shootWeapon.getClass().getName().equals(Machinegun.class.getName())) {
             Vector2 shootDirection = new Vector2(toPlayer.x, toPlayer.y).setLength2(1).scl(rad * PPM);
             shootWeapon.shoot(shootFrom, shootDirection);
